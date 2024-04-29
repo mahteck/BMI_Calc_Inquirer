6b1e0f322b82c211f6482b9d0f3c7958c1a2d583
mshoaib1244 <shoaib1244@gmail.com>
2024-04-29 11:58:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// let weightInKg = 70;
+// let heightInMeters = 1.75;
+// let bmi = weightInKg / (heightInMeters * heightInMeters);
+import inquirer from "inquirer";
+const numb1 = await inquirer.prompt({
+    name: "weightInKg",
+    type: "number",
+    message: "Enter Weight In Kg"
+});
+const numb2 = await inquirer.prompt({
+    name: "heightInMeters",
+    type: "number",
+    message: "enter height in meters"
+});
+let result = numb1.weightInKg / (numb2.heightInMeters * numb2.heightInMeters);
+console.log(`your bmi result is ${result}`);
